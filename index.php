249ff51870dcd503ed412c658825a8938da4f93b
Carolina Burgos Anillo <66441394+cmba-alt@users.noreply.github.com>
2021-09-23 07:18:42 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -22,37 +22,7 @@ else {$session_status = 'Cerrar sesión';
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <nav class="navbar navbar-customclass navbar-expand-lg">
-        <a class="navbar-brand" href="index.php">Smart Shelf</a>
-        <a class="navbar-brand" href="#">Maxi Despensa</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="productos.php">Productos <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="promociones.php">Promociones</a>
-                </li>
-                <?php if ($loggedin == 0){
-                    echo ("<li class='nav-item disabled'>
-                    <a class='nav-link' href='login.php'>Inicio de Sesión</a>
-                </li>" );
-                }?> 
-            </ul>
-        </div>
-        <?php
-            if ($loggedin == 0) {
-                echo(htmlentities($session_status));
-            }
-            else {
-                echo("<a class='nav-link' href='logout.php'>$session_status</a>");
-            }
-            
-        ?>
-    </nav>
+<?php include("navbar.php")?>
 
 <div id="title-box">
     <h1>Smart Shelf</h1>
