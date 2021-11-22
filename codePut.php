8d566ce7aa23cf9356f46c21b9e81e9eb656bdd4
Juan Sebastián Barreto Jiménez <juan_barreto@javeriana.edu.co>
2021-11-22 07:38:13 -0500

    TO thingspeak

--- codePut.php
+++ codePut.php
@@ -21,6 +21,35 @@
     $ref_table = "/Tiendas/MaxiDespensa/Productos/$token";
     $database->getReference($ref_table)->update($postData);
     $_SESSION['success'] = "Producto Actualizado";
+    $response = file_get_contents('https://api.thingspeak.com/update?api_key=A444YRSEAZKLS3KB&field1=1');
+
+    include('includes/GFirestone.php');
+    $store='MaxiDespensa';
+    $fs = new GFirestone($store);
+    $products = $fs->getProductos();
+    $token = '/t/';
+    foreach ($products as $prod){
+        //$message = ";
+        $message = $prod["Cantidad"].$token.$prod["Fabricante"].$token.$prod["Id"].$token.$prod["Imagen"].$token.$prod["Nombre"].$token.$prod["Precio"].$token.$prod["Tamano"].$token.$prod["UnidadMedida"];
+        $url = 'https://api.thingspeak.com/update.json?api_key=75RJHS14YYDV4XEU&/json';
+        print($message);
+
+        $data = [ 'field1' => $message];
+
+        $options = array(
+            'http' => array(
+            'method'  => 'GET',
+            'content' => json_encode( $data ),
+            'header'=>  "Content-Type: application/json\r\n" .
+                        "Accept: application/json\r\n"
+            )
+        );  
+        $context  = stream_context_create( $options );
+        $result = file_get_contents( $url, false, $context );
+        //$response = json_decode( $result );
+        sleep(20);
+    }
+
     header('Location: products.php');
     return;
 
